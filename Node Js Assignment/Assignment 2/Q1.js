522e4e760c59874fad955f0924ae979873b3368c
k-madani <krishnamadani@gmail.com>
2021-01-18 13:04:23 +0530

    initial commit

---
+++ Node Js Assignment/Assignment 2/Q1.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/', (req,res)=>{
+    res.send('Hello World!');
+});
+
+app.listen(3000,function(){
+    console.log("Server connected");
+})
---
+++ Node Js Assignment/Assignment 2/Q2.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/year',(req,res)=>{
+    let age = req.query.age;
+    var date = new Date();
+    let thisYear = date.getFullYear();
+    let bornYear = thisYear-age;
+    res.send(`You were born in ${bornYear}`);
+});
+
+app.listen(3000,function(){
+    console.log("Server connected");
+})
